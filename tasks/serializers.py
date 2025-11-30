97c938bf3eab861428bc141004defb5a4937dd64
Nanda <nandakumarreddy6478@gmail.com>
2025-11-30 20:27:12 +0530

    fix(tasks): add TaskSerializer to support save, fetch and delete endpoints

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,37 +1,14 @@
 from rest_framework import serializers
 from .models import Task
 
-
-class TaskInputSerializer(serializers.Serializer):
-    id = serializers.IntegerField(required=False)
-    title = serializers.CharField()
-    due_date = serializers.CharField(required=False, allow_null=True, allow_blank=True)
-    importance = serializers.IntegerField(required=False, default=5)
-    estimated_hours = serializers.IntegerField(required=False, default=1)
-    dependencies = serializers.ListField(
-        child=serializers.IntegerField(),
-        required=False,
-        default=list
-    )
-
-
-class StrategySerializer(serializers.Serializer):
-    strategy = serializers.CharField()
-    tasks = TaskInputSerializer(many=True)
-
-
-class TaskOutputSerializer(serializers.Serializer):
-    id = serializers.IntegerField(required=False)
-    title = serializers.CharField()
-    due_date = serializers.CharField(required=False, allow_null=True)
-    importance = serializers.IntegerField()
-    estimated_hours = serializers.IntegerField()
-    dependencies = serializers.ListField(child=serializers.IntegerField())
-    score = serializers.FloatField()
-    explanation = serializers.CharField()
-
-
-class TaskModelSerializer(serializers.ModelSerializer):
+class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = "__all__"
+
+class TaskInputSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    due_date = serializers.DateField()
+    importance = serializers.IntegerField(min_value=1, max_value=10)
+    estimated_hours = serializers.IntegerField(min_value=1)
+    dependencies = serializers.ListField(child=serializers.IntegerField(), required=False)
