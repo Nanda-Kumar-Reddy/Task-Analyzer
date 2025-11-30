7efe047bff22f58aff939ae8d3db4f5a07f5e1df
Nanda <nandakumarreddy6478@gmail.com>
2025-11-30 11:14:08 +0530

    feat(tasks): add DRF serializers for task input and output

---
+++ tasks/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+class TaskInputSerializer(serializers.Serializer):
+    id = serializers.IntegerField(required=False)
+    title = serializers.CharField()
+    due_date = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    importance = serializers.IntegerField(required=False, default=5)
+    estimated_hours = serializers.IntegerField(required=False, default=1)
+    dependencies = serializers.ListField(
+        child=serializers.IntegerField(),
+        required=False,
+        default=list
+    )
+
+class StrategySerializer(serializers.Serializer):
+    strategy = serializers.CharField()
+    tasks = TaskInputSerializer(many=True)
+
+class TaskOutputSerializer(serializers.Serializer):
+    id = serializers.IntegerField(required=False)
+    title = serializers.CharField()
+    due_date = serializers.CharField(required=False, allow_null=True)
+    importance = serializers.IntegerField()
+    estimated_hours = serializers.IntegerField()
+    dependencies = serializers.ListField(child=serializers.IntegerField())
+    score = serializers.FloatField()
+    explanation = serializers.CharField()
